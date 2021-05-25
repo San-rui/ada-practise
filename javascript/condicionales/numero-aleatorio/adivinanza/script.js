1632a19175e2911d274fffc95d09d8c632c7ea09
San <sruidiaz@hotmail.com>
2021-05-25 10:51:41 +0200

    Style

--- javascript/condicionales/numero-aleatorio/adivinanza/script.js
+++ javascript/condicionales/numero-aleatorio/adivinanza/script.js
@@ -4,11 +4,10 @@ const riddle3 = document.querySelector('#riddle3');
 const riddle4 = document.querySelector('#riddle4');
 const riddle5 = document.querySelector('#riddle5');
 const play =document.querySelector('#play');
-
 let answerX=""
 
 const rangeNum= Math.ceil(Math.random() * 5)
-    const randomNum=Math.round(rangeNum);
+const randomNum=Math.round(rangeNum);
 
 const playRiddle=()=>{
     
@@ -16,40 +15,22 @@ const playRiddle=()=>{
     if(randomNum=="1"){
         answerX="elefante";
         riddle1.style=`display:flex`;
-        riddle2.style=`display:none`;
-        riddle3.style=`display:none`;
-        riddle4.style=`display:none`;
-        riddle5.style=`display:none`;
-        
     }
     else if(randomNum=="2"){
         answerX="pato";
         riddle2.style=`display:flex`;
-        riddle1.style=`display:none`;
-        riddle3.style=`display:none`;
-        riddle4.style=`display:none`;
-        riddle5.style=`display:none`;
     }
     else if(randomNum=="3"){
+        answerX="murcielago";
         riddle3.style=`display:flex`;
-        riddle1.style=`display:none`;
-        riddle2.style=`display:none`;
-        riddle4.style=`display:none`;
-        riddle5.style=`display:none`;
     }
     else if(randomNum=="4"){
+        answerX="pingüino";
         riddle4.style=`display:flex`;
-        riddle1.style=`display:none`;
-        riddle2.style=`display:none`;
-        riddle3.style=`display:none`;
-        riddle5.style=`display:none`;
     }
     else if(randomNum=="5"){
+        answerX="rana";
         riddle5.style=`display:flex`;
-        riddle1.style=`display:none`;
-        riddle2.style=`display:none`;
-        riddle3.style=`display:none`;
-        riddle4.style=`display:none`;
     }
 }
 
@@ -57,6 +38,9 @@ play.addEventListener('click', playRiddle);
 
 const answerRiddle1= document.querySelector('#answerRiddle1');
 const answerRiddle2= document.querySelector('#answerRiddle2');
+const answerRiddle3= document.querySelector('#answerRiddle3');
+const answerRiddle4= document.querySelector('#answerRiddle4');
+const answerRiddle5= document.querySelector('#answerRiddle5');
 const text = document.querySelector('#text')
 
 const respuesta =()=>{
@@ -66,22 +50,28 @@ const respuesta =()=>{
     else if(answerRiddle2.value==answerX && randomNum=="2"){
         text.innerHTML="Correcto!!!"
     }
+    else if(answerRiddle3.value==answerX && randomNum=="3"){
+        text.innerHTML="Correcto!!!"
+    }
+    else if(answerRiddle4.value==answerX && randomNum=="4"){
+        text.innerHTML="Correcto!!!"
+    }
+    else if(answerRiddle5.value==answerX && randomNum=="5"){
+        text.innerHTML="Correcto!!!"
+    }
     else{
         text.innerHTML="Vuelve a intentarlo"
     }
 }
-
 answerRiddle1.addEventListener('change' , respuesta);
 answerRiddle2.addEventListener('change' , respuesta);
-
-
-
-
+answerRiddle3.addEventListener('change' , respuesta);
+answerRiddle4.addEventListener('change' , respuesta);
+answerRiddle5.addEventListener('change' , respuesta);
 
 const butonReset = document.querySelector('#reset');
 
 function reloadPage(){
     window.location.reload();
 }
-
 butonReset.addEventListener('click', reloadPage)
